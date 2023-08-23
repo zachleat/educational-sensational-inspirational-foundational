d29a18e7977da32cec8dbc66b0b97023dc8d5fd3
Zach Leatherman <zachleatherman@gmail.com>
2023-08-23 17:24:59 -0500

    Prepare for Video Embed

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,12 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
 	eleventyConfig.ignores.add("src/_schemas/*");
 
-	eleventyConfig.addPassthroughCopy("./src/style.css");
-	eleventyConfig.addPassthroughCopy("./src/logo.png");
+	eleventyConfig.addPassthroughCopy("./src/*.{css,png}");
+	eleventyConfig.addPassthroughCopy({
+		"node_modules/@11ty/is-land/is-land.js": "public/is-land.js",
+		"node_modules/lite-youtube-embed/src/lite-yt-embed.js": "public/lite-yt-embed.js",
+		"node_modules/lite-youtube-embed/src/lite-yt-embed.css": "public/lite-yt-embed.css"
+	});
 
 	eleventyConfig.addPlugin(pluginRss);
 
@@ -19,9 +23,23 @@ module.exports = function(eleventyConfig) {
 		return date.getFullYear();
 	});
 
+	eleventyConfig.addShortcode("embedYouTube", function(slug, label) {
+		let fallback = `https://i.ytimg.com/vi/${slug}/maxresdefault.jpg`;
+
+		return `<is-land on:visible>
+	<template data-island="once">
+		<link rel="stylesheet" href="/public/lite-yt-embed.css">
+		<script src="/public/lite-yt-embed.js"></script>
+	</template>
+	<lite-youtube videoid="${slug}" playlabel="Play${label ? `: ${label}` : ""}" style="background-image:url('${fallback}')">
+		<a href="https://youtube.com/watch?v=${slug}" class="lty-playbtn" title="Play Video"><span class="lyt-visually-hidden">Play Video${label ? `: ${label}` : ""}</span></a>
+	</lite-youtube>
+</is-land>`;
+	});
+
 	return {
-    dir: {
-      input: "src",
-    },
-  };
+		dir: {
+			input: "src",
+		},
+	};
 };
